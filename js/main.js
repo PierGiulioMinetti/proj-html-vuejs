3cb695fe2fa33da058efe79f97e9719aa9d450dc
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-11-30 12:48:38 +0100

    Starting

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+      linkHeader: [
+          {
+              link: 'Home'
+          },
+          {
+            link: 'Pages'
+           },
+           {
+            link: 'Courses'
+           },
+           {
+            link: 'Features'
+           },
+           {
+            link: 'Blog'
+           },
+           {
+            link: 'Shop'
+           },
+          
+      ],
+    }
+
+
+  }) //--> end vueJS
