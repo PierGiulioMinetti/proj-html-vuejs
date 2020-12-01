91fa9bcee3575f4f2753b83cf1ac6fd281c16995
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-12-01 17:37:37 +0100

    Need Styling

--- js/main.js
+++ js/main.js
@@ -20,9 +20,41 @@ const app = new Vue({
            {
             link: 'BLOG'
            },
-          
-      ],
-    },
+        ],
+      
+        footer: [
+            {
+              link: 'Copyright 2012 - 2020',
+              bordo: 'I'
+            },
+            {
+              link: 'Avada Theme by Theme Fusion',
+              bordo: 'I'
+            },
+            {
+              link: 'All Right Reserved',
+              bordo: 'I'
+            },
+            {
+              link: 'Powered by WordPress'
+            }
+          ],
+
+        social: [
+            {
+              nome: 'fab fa-facebook-f',
+            },
+            {
+              nome: 'fab fa-twitter'
+            },
+            {
+              nome: 'fab fa-instagram'
+            }
+
+        ]
+
+
+    }, //-->end data
     
 
 
